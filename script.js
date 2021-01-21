cb3b97d6b6baabf102009ab5e1c4522da84f6fb3
Amitai Levi <amitailevi86@gmail.com>
2021-01-21 14:06:37 +0200

    settings and data structure

---
+++ script.js
@@ -0,0 +1,113 @@
+//data construction
+let elementArray = [
+  {
+    startedAt: new Date("2021-01-20:13:00"),
+
+    finishedAt: new Date("2021-01-20:19:00"),
+
+    tasksGiven: 10,
+
+    tasksFinished: 7,
+
+    topic: JavaScript,
+  },
+  {
+    startedAt: new Date("2021-01-19:9:00"),
+
+    finishedAt: new Date("2021-01-19:17:00"),
+
+    tasksGiven: 20,
+
+    tasksFinished: 13,
+
+    topic: CSS,
+  },
+  {
+    startedAt: new Date("2021-01-18:18:00"),
+
+    finishedAt: new Date("2021-01-18:20:00"),
+
+    tasksGiven: 15,
+
+    tasksFinished: 15,
+
+    topic: Arrays,
+  },
+  {
+    startedAt: new Date("2021-01-17:16:00"),
+
+    finishedAt: new Date("2021-01-17:21:00"),
+
+    tasksGiven: 25,
+
+    tasksFinished: 18,
+
+    topic: Elements,
+  },
+  {
+    startedAt: new Date("2021-01-16:13:00"),
+
+    finishedAt: new Date("2021-01-16:19:00"),
+
+    tasksGiven: 32,
+
+    tasksFinished: 30,
+
+    topic: HTML,
+  },
+  {
+    startedAt: new Date("2021-01-15:10:00"),
+
+    finishedAt: new Date("2021-01-15:19:30"),
+
+    tasksGiven: 22,
+
+    tasksFinished: 19,
+
+    topic: DOM,
+  },
+  {
+    startedAt: new Date("2021-01-14:11:00"),
+
+    finishedAt: new Date("2021-01-14:13:20"),
+
+    tasksGiven: 10,
+
+    tasksFinished: 2,
+
+    topic: WEB,
+  },
+  {
+    startedAt: new Date("2021-01-13:12:00"),
+
+    finishedAt: new Date("2021-01-13:21:00"),
+
+    tasksGiven: 18,
+
+    tasksFinished: 9,
+
+    topic: FrontEnd,
+  },
+  {
+    startedAt: new Date("2021-01-12:12:30"),
+
+    finishedAt: new Date("2021-01-12:16:15"),
+
+    tasksGiven: 11,
+
+    tasksFinished: 7,
+
+    topic: Theory,
+  },
+  {
+    startedAt: new Date("2021-01-11:13:45"),
+
+    finishedAt: new Date("2021-01-11:19:00"),
+
+    tasksGiven: 16,
+
+    tasksFinished: 16,
+
+    topic: Introduction,
+  },
+];
