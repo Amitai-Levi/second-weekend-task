8fce51f2364619e275786b4c811e7200b2e305db
Amitai Levi <amitailevi86@gmail.com>
2021-01-21 15:50:56 +0200

    finished js html structure

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let elementArray = [
 
     tasksFinished: 7,
 
-    topic: JavaScript,
+    topic: "JavaScript",
   },
   {
     startedAt: new Date("2021-01-19:9:00"),
@@ -20,7 +20,7 @@ let elementArray = [
 
     tasksFinished: 13,
 
-    topic: CSS,
+    topic: "CSS",
   },
   {
     startedAt: new Date("2021-01-18:18:00"),
@@ -31,7 +31,7 @@ let elementArray = [
 
     tasksFinished: 15,
 
-    topic: Arrays,
+    topic: "Arrays",
   },
   {
     startedAt: new Date("2021-01-17:16:00"),
@@ -42,7 +42,7 @@ let elementArray = [
 
     tasksFinished: 18,
 
-    topic: Elements,
+    topic: "Elements",
   },
   {
     startedAt: new Date("2021-01-16:13:00"),
@@ -53,7 +53,7 @@ let elementArray = [
 
     tasksFinished: 30,
 
-    topic: HTML,
+    topic: "HTML",
   },
   {
     startedAt: new Date("2021-01-15:10:00"),
@@ -64,7 +64,7 @@ let elementArray = [
 
     tasksFinished: 19,
 
-    topic: DOM,
+    topic: "DOM",
   },
   {
     startedAt: new Date("2021-01-14:11:00"),
@@ -75,7 +75,7 @@ let elementArray = [
 
     tasksFinished: 2,
 
-    topic: WEB,
+    topic: "WEB",
   },
   {
     startedAt: new Date("2021-01-13:12:00"),
@@ -86,7 +86,7 @@ let elementArray = [
 
     tasksFinished: 9,
 
-    topic: FrontEnd,
+    topic: "FrontEnd",
   },
   {
     startedAt: new Date("2021-01-12:12:30"),
@@ -97,7 +97,7 @@ let elementArray = [
 
     tasksFinished: 7,
 
-    topic: Theory,
+    topic: "Theory",
   },
   {
     startedAt: new Date("2021-01-11:13:45"),
@@ -108,6 +108,70 @@ let elementArray = [
 
     tasksFinished: 16,
 
-    topic: Introduction,
+    topic: "Introduction",
   },
 ];
+// a loop that calculates the 2 extra properties:
+for (const element of elementArray) {
+  element["Total Time Spent"] = element.finishedAt - element.startedAt;
+  element["tasksFinishedPrecent"] = Math.floor(
+    (element.tasksFinished * 100) / element.tasksGiven
+  );
+}
+//TABLE STRUCTURE
+
+// start of the table
+document.write("<table>");
+//content
+
+//header
+document.write("<tr>");
+for (let prop in elementArray[0]) {
+  document.write("<th>" + prop + "</th>");
+}
+document.write("</tr>");
+//content
+for (const element of elementArray) {
+  document.write("<tr>");
+  for (const prop in element) {
+    switch (prop) {
+      case "startedAt":
+      case "finishedAt":
+        document.write(
+          "<td>" +
+            pad0(element[prop].getHours()) +
+            ":" +
+            pad0(element[prop].getMinutes()) +
+            "</td>"
+        );
+        break;
+      case "Total Time Spent":
+        document.write("<td>" + toTime(element[prop]) + "</td>");
+        break;
+      case "tasksFinishedPrecent":
+        document.write("<td>" + element[prop] + "%</td>");
+        break;
+      default:
+        document.write("<td>" + element[prop] + "</td>");
+    }
+  }
+  document.write("</tr>");
+}
+
+// end of the table
+document.write("</table>");
+
+// the following function is for formatting the time to look properly
+function pad0(num) {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num;
+}
+
+//the following function turn miliseconds to hours
+function toTime(mili) {
+  let hour = Math.floor(mili / 3600000);
+  let minutes = (mili % 3600000) / 60000;
+  return pad0(hour) + ":" + pad0(minutes);
+}
