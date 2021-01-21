1b794cbea84983d28e9e7de508934a98d148ee9c
Amitai Levi <amitailevi86@gmail.com>
2021-01-21 16:45:34 +0200

    basic CSS and dinamic color columns

--- script.js
+++ script.js
@@ -111,67 +111,112 @@ let elementArray = [
     topic: "Introduction",
   },
 ];
-// a loop that calculates the 2 extra properties:
-for (const element of elementArray) {
-  element["Total Time Spent"] = element.finishedAt - element.startedAt;
-  element["tasksFinishedPrecent"] = Math.floor(
-    (element.tasksFinished * 100) / element.tasksGiven
-  );
-}
-//TABLE STRUCTURE
+function table() {
+  // a loop that calculates the 2 extra properties:
+  for (const element of elementArray) {
+    element["Total Time Spent"] = element.finishedAt - element.startedAt;
+    element["tasksFinishedPrecent"] = Math.floor(
+      (element.tasksFinished * 100) / element.tasksGiven
+    );
+  }
+  //TABLE STRUCTURE
 
-// start of the table
-document.write("<table>");
-//content
+  // start of the table
+  document.write("<table>");
+  //content
 
-//header
-document.write("<tr>");
-for (let prop in elementArray[0]) {
-  document.write("<th>" + prop + "</th>");
-}
-document.write("</tr>");
-//content
-for (const element of elementArray) {
+  //header
   document.write("<tr>");
-  for (const prop in element) {
-    switch (prop) {
-      case "startedAt":
-      case "finishedAt":
-        document.write(
-          "<td>" +
-            pad0(element[prop].getHours()) +
-            ":" +
-            pad0(element[prop].getMinutes()) +
-            "</td>"
-        );
-        break;
-      case "Total Time Spent":
-        document.write("<td>" + toTime(element[prop]) + "</td>");
-        break;
-      case "tasksFinishedPrecent":
-        document.write("<td>" + element[prop] + "%</td>");
-        break;
-      default:
-        document.write("<td>" + element[prop] + "</td>");
-    }
+  for (let prop in elementArray[0]) {
+    document.write("<th>" + prop + "</th>");
   }
   document.write("</tr>");
-}
-
-// end of the table
-document.write("</table>");
-
-// the following function is for formatting the time to look properly
-function pad0(num) {
-  if (num < 10) {
-    num = "0" + num;
+  //content
+  for (const element of elementArray) {
+    document.write("<tr>");
+    for (const prop in element) {
+      switch (prop) {
+        case "startedAt":
+        case "finishedAt":
+          document.write(
+            "<td>" +
+              pad0(element[prop].getHours()) +
+              ":" +
+              pad0(element[prop].getMinutes()) +
+              "</td>"
+          );
+          break;
+        case "Total Time Spent":
+          document.write("<td class='time'>" + toTime(element[prop]) + "</td>");
+          break;
+        case "tasksFinishedPrecent":
+          document.write("<td class='precent'>" + element[prop] + "%</td>");
+          break;
+        default:
+          document.write("<td>" + element[prop] + "</td>");
+      }
+    }
+    document.write("</tr>");
   }
-  return num;
-}
 
-//the following function turn miliseconds to hours
-function toTime(mili) {
-  let hour = Math.floor(mili / 3600000);
-  let minutes = (mili % 3600000) / 60000;
-  return pad0(hour) + ":" + pad0(minutes);
+  // end of the table
+  document.write("</table>");
+
+  // the following function is for formatting the time to look properly
+  function pad0(num) {
+    if (num < 10) {
+      num = "0" + num;
+      8;
+    }
+    return num;
+  }
+
+  //the following function turn miliseconds to hours
+  function toTime(mili) {
+    //   let hour = Math.floor(mili / 3600000);
+    //   let minutes = (mili % 3600000) / 60000;
+    //   return pad0(hour) + ":" + pad0(minutes);
+    return Math.floor(mili / 36000) / 100;
+  }
+  // making the column "Total time spent" colorful
+  const timeList = document.querySelectorAll(".time");
+  for (const timeCell of timeList) {
+    let cellValue = parseInt(timeCell.textContent);
+    switch (true) {
+      case cellValue <= 2:
+        timeCell.style.backgroundColor = "green";
+        break;
+      case cellValue > 2 && cellValue <= 5:
+        timeCell.style.backgroundColor = "yellow";
+        break;
+
+      case cellValue > 5 && cellValue <= 8:
+        timeCell.style.backgroundColor = "orange";
+        break;
+
+      default:
+        timeCell.style.backgroundColor = "red";
+    }
+  }
+
+  // making the column "TaskFinishePercent" colorful
+  const precentList = document.querySelectorAll(".precent");
+  for (const precentCell of precentList) {
+    let presentCellValue = parseInt(precentCell.textContent);
+    switch (true) {
+      case presentCellValue <= 20:
+        precentCell.style.backgroundColor = "red";
+        break;
+      case presentCellValue > 20 && presentCellValue <= 50:
+        precentCell.style.backgroundColor = "orange";
+        break;
+
+      case presentCellValue > 50 && presentCellValue <= 80:
+        precentCell.style.backgroundColor = "yellow";
+        break;
+
+      default:
+        precentCell.style.backgroundColor = "green";
+    }
+  }
 }
